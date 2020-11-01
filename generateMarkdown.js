27500f0baeda64889dcd198b38913e6e1660d0cd
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-11-01 16:24:40 -0500

    Created template literal for readme md.

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,40 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
+const generateMarkdown = (data) => {
   return `# ${data.title}
+  ## Description
+  ${data.description}
+## Table of Contents
 
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#usage)
+* [Test](#test)
+* [Feedback](#feedback)
+* [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Test
+
+${data.test}
+
+## Questions
+
+${data.questions}
+
+## License
+
+// ${data.licence}
 `;
 }
 
